2c7ced9539c01124a7c522286dd96a3bdc94692c
marcofarmscorp <dceglobalpartners@gmail.com>
2026-08-26 12:53:03 -0500

    Update WhatsApp link and company name in contact.ts

--- config/contact.ts
+++ config/contact.ts
@@ -1,7 +1,7 @@
 export const contact = {
   whatsappNumber: "+1 (305) 384-8691",
   whatsappLink:
-    "https://wa.me/13053848691?text=Hello%20Marco%20Farms%2C%20I%27d%20like%20to%20request%20a%20quote.",
+    "https://wa.me/13053848691?text=Hello%20Marcos%20Farms%20Corp.%2C%20I%27d%20like%20to%20request%20a%20quote.",
   email: "agromarcosfarm@gmail.com",
   emailLink: "mailto:agromarcosfarm@gmail.com",
 };
